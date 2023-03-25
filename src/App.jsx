ba90c87272d53ac7a8addbd72ffbb441ad4753ed
ealtugkirman <ealtugkirman@gmail.com>
2023-03-25 16:02:47 +0300

    width fixed3

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import condom from "./assets/condom.png";
 
 export default function App() {
   return (
-    <div className="selector w-auto">
+    <div className="selector">
       <Art />
       <Banner />
       <Navbar />
